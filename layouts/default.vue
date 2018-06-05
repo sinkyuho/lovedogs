ce1a66cc87a87ab95082c8a62544f07dce627d30
sinkyuho <bb7305@naver.com>
2018-06-05 10:47:20 +0900

    html doglove

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,26 @@
-
 <template>
-    <div>
-      <nav class="navbar">
-        <div class="navbar-brand">
-          <nuxt-link to="/" class="navbar-item">개사랑</nuxt-link>
+  <div>
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <nuxt-link to="/" class="navbar-item">
+          <figure class="image is-64x64">
+            <img src="~static/개.png" alt="">
+          </figure>
+        </nuxt-link>
+      </div>
+      <div class="navbar-menu is-active">
+        <div class="navbar-end">
+          <nuxt-link to="/stat" class="navbar-item">통계</nuxt-link>
+          <nuxt-link to="/about" class="navbar-item">소개</nuxt-link>
         </div>
-        <div class="navbar-menu is-active">
-          <div class="navbar-end">
-                <nuxt-link to="/about" class="navbar-item">소개</nuxt-link>
-          </div>
-        </div>
-      </nav>
-      <nuxt/>
-      <footer class="footer has-text-centered">
-        <p>2018년 목원대학교 개사랑. 모든 저작권은 개사랑에 있습니다.</p>
-      </footer>
-    </div>
-  </template>
+      </div>
+    </nav>
+    <nuxt/>
+    <footer class="footer has-text-centered">
+      <div>
+        <figure class="image is-32x32">
+          <img src="~assets/개발.png" alt="">
+        </figure> 2018년 목원대학교 개사랑. 모든 저작권은 개사랑에 있습니다.</div>
+    </footer>
+  </div>
+</template>
